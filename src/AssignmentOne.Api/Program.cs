27ae2a11e8bd2ef1aa075de21ea34a34ded74a8f
Dmytro <chepil.dmitriy@gmail.com>
2026-06-21 14:25:12 -0700

    added migration run on startup

--- src/AssignmentOne.Api/Program.cs
+++ src/AssignmentOne.Api/Program.cs
@@ -26,6 +26,12 @@ builder.Services.AddScoped<IVideoGameService, VideoGameService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseHttpsRedirection();
 app.UseCors();
 app.MapControllers();
